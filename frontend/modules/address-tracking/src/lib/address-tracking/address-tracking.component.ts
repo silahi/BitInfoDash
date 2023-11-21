5fb3eeaf894e8188b6e2d1baaf281f81bf9339e1
Ali Houssene <silahi.alihoussene@gmail.com>
2023-11-21 17:57:21 +0000

    Add address-tracking module

---
+++ frontend/modules/address-tracking/src/lib/address-tracking/address-tracking.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'bit-info-dash-address-tracking',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './address-tracking.component.html',
+  styleUrls: ['./address-tracking.component.css'],
+})
+export class AddressTrackingComponent {}
