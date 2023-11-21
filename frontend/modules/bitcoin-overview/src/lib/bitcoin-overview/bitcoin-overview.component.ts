52788d5bb0b8c5e503257634c77eaef0b5cdd32d
Ali Houssene <silahi.alihoussene@gmail.com>
2023-11-21 17:50:40 +0000

    Add CryptoDataService  to handle the API requests and Fetch Data

--- frontend/modules/bitcoin-overview/src/lib/bitcoin-overview/bitcoin-overview.component.ts
+++ frontend/modules/bitcoin-overview/src/lib/bitcoin-overview/bitcoin-overview.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CryptoData } from '../shared/models/crypto-data.model';
+import { CryptoDataService } from '../shared/models/services/crypto-data.service';
 
 @Component({
   selector: 'bit-info-dash-bitcoin-overview',
@@ -8,4 +10,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './bitcoin-overview.component.html',
   styleUrls: ['./bitcoin-overview.component.css'],
 })
-export class BitcoinOverviewComponent {}
+
+export class BitcoinOverviewComponent implements OnInit {
+  cryptoData!: CryptoData;
+
+  constructor(private cryptoDataService: CryptoDataService) { }
+
+  ngOnInit(): void {
+     
+  }   
+}
