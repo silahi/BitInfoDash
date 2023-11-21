4ffd985259d6679dc8f7474ac7c4728f1712eb4a
Ali Houssene <silahi.alihoussene@gmail.com>
2023-11-21 18:03:40 +0000

    Link the modules

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,5 +10,25 @@ export const appRoutes: Route[] = [
         path: "bitcoin-overview",
         loadComponent: () =>
             import('@bit-info-dash/bitcoin-overview').then(c => c.BitcoinOverviewComponent),
-    }
+    },
+    {
+        path: "blockchain-analysis",
+        loadComponent: () =>
+            import('@bit-info-dash/blockchain-analysis').then(c => c.BlockchainAnalysisComponent),
+    },
+    {
+        path: "address-tracking",
+        loadComponent: () =>
+            import('@bit-info-dash/address-tracking').then(c => c.AddressTrackingComponent),
+    },
+    {
+        path: "market-trends",
+        loadComponent: () =>
+            import('@bit-info-dash/market-trends').then(c => c.MarketTrendsComponent),
+    },
+    {
+        path: "network-security",
+        loadComponent: () =>
+            import('@bit-info-dash/network-security').then(c => c.NetworkSecurityComponent),
+    },
 ];
