b66e50d6735c72be293cc940bcddccab2604286a
Ali Houssene <silahi.alihoussene@gmail.com>
2023-11-22 06:58:48 +0000

    Add a shared module

---
+++ frontend/modules/shared/src/lib/shared/shared.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'bit-info-dash-shared',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shared.component.html',
+  styleUrls: ['./shared.component.css'],
+})
+export class SharedComponent {}
