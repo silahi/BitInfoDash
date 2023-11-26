11cfac93e19230c25fa5aeaa1678d5eef3a81e7e
Ali Houssene <silahi.alihoussene@baamtu.com>
2023-11-26 21:10:57 +0000

    Adjust the home page

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
@@ -7,5 +7,6 @@ import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [provideClientHydration(),
   importProvidersFrom(HttpClientModule),
+  provideHttpClient(withFetch()),
   provideRouter(appRoutes)],
 };
