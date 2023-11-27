0f1b4ce22fc4beac4ee2e1fe4d64f3e6b74e1278
Ali Houssene <silahi.alihoussene@gmail.com>
2023-11-27 16:13:05 +0000

    Adding address dialog component

--- frontend/modules/address-tracking/src/lib/address-tracking/address-tracking.component.ts
+++ frontend/modules/address-tracking/src/lib/address-tracking/address-tracking.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { AddressDialogComponent } from './address-dialog/address-dialog.component';
 
 @Component({
   selector: 'bit-info-dash-address-tracking',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatDialogModule],
   templateUrl: './address-tracking.component.html',
   styleUrls: ['./address-tracking.component.css'],
 })
-export class AddressTrackingComponent {}
+export class AddressTrackingComponent {
+  constructor(public dialog: MatDialog) { }
+  openDialog() {
+    const dialogRef = this.dialog.open(AddressDialogComponent, {
+      height: '300px',
+      width: '70%',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
